003d131cd4cd5b9ffc7db221586d1d3968108491
lelegoyeng <lelegoyengs@gmail.com>
2025-02-13 18:32:47 +0700

    Update vercell

---
+++ .env,example.js
@@ -0,0 +1,2 @@
+SUPABASE_URL=<Your Supabase URL>
+SUPABASE_ANON_KEY=<Your Supabase Anon Key>
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private supabase: SupabaseClient;
+
+  constructor(private configService: ConfigService) {
+    const url = this.configService.get<string>('SUPABASE_URL');
+    const key = this.configService.get<string>('SUPABASE_ANON_KEY');
+    this.supabase = createClient(url, key);
+  }
+
+  async getData() {
+    const { data, error } = await this.supabase.from('your_table').select('*');
+    if (error) throw new Error(error.message);
+    return data;
   }
 }
