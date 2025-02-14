be4735c64d5df22acee2637ac6b95644755d8d29
lelegoyeng <lelegoyengs@gmail.com>
2025-02-14 10:45:11 +0700

    Update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Param } from '@nestjs/common';
-import { ApiTags, ApiOperation, ApiParam } from '@nestjs/swagger';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiParam, ApiBody } from '@nestjs/swagger';
 import { AppService } from './app.service';
+import { Jenis, Kategori } from '@prisma/client';
 
 @ApiTags('Transaction')
 @Controller()
@@ -16,5 +17,49 @@ export class AppController {
   })
   async getPrismaData(@Param('date') date: string) {
     return this.appService.getDataFromPrisma(date);
+  }
+
+  @Post('/transaction')
+  @ApiOperation({ summary: 'Create a new transaction' })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        tanggal: {
+          type: 'string',
+          format: 'date-time',
+          example: '2025-02-06T10:15:30.000Z',
+        },
+        nominal: { type: 'number', example: 200000 },
+        keterangan: { type: 'string', example: 'ditransfer masuk' },
+        kategori: { type: 'string', example: 'TRANSFER' },
+        jenis: { type: 'string', example: 'PENDAPATAN' },
+        createdAt: {
+          type: 'string',
+          format: 'date-time',
+          example: '2025-02-06T10:15:48.000Z',
+        },
+        updatedAt: {
+          type: 'string',
+          format: 'date-time',
+          example: '2025-02-06T10:16:03.000Z',
+        },
+      },
+      required: ['tanggal', 'nominal', 'keterangan', 'kategori', 'jenis'],
+    },
+  })
+  async createTransaction(
+    @Body()
+    data: {
+      tanggal: Date;
+      nominal: number;
+      keterangan: string;
+      kategori: Kategori;
+      jenis: Jenis;
+      createdAt: Date;
+      updatedAt: Date;
+    },
+  ) {
+    return this.appService.createTransaction(data);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+import { Jenis, Kategori } from '@prisma/client';
 
 @Injectable()
 export class AppService {
@@ -8,5 +9,22 @@ export class AppService {
   async getDataFromPrisma(date: string) {
     console.log(date);
     return this.prisma.transaction.findMany();
+  }
+
+  async createTransaction(data: {
+    tanggal: Date;
+    nominal: number;
+    keterangan: string;
+    kategori: Kategori;
+    jenis: Jenis;
+    createdAt?: Date;
+    updatedAt?: Date;
+  }) {
+    try {
+      const result = await this.prisma.transaction.create({ data });
+      return result;
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
