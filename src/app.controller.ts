c449ee31b6b3584be04db68d5ee03f278a61ed71
lelegoyeng <lelegoyengs@gmail.com>
2025-02-14 10:27:25 +0700

    Update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,19 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiParam } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
+@ApiTags('Transaction')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('/transaction/:date')
+  @ApiParam({
+    name: 'date',
+    required: true,
+    example: '2024-02-14',
+    description: 'Tanggal Transaksi',
+  })
   async getPrismaData(@Param('date') date: string) {
     return this.appService.getDataFromPrisma(date);
   }
